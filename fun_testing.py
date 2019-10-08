e2dd23651ba17d3866947220330cf4359f445348
budfox3 <silvano.marchesi@uzh>
2019-10-08 09:29:56 +0200

    update

---
+++ fun_testing.py
@@ -0,0 +1,43 @@
+#%%
+import numpy as np
+import pandas as pd
+import json
+
+import scipy.optimize
+import scipy as sp
+import scipy.stats as stat
+import matplotlib.pyplot as plt
+
+import importlib
+import datapreprocessing
+import data
+
+
+import time
+
+
+#%% Load the data 
+importlib.reload(datapreprocessing)
+# importlib.reload(data)
+path = 'C:/Users/silva/iCloudDrive/Docs/Ausbildung/QuantLibrary/MScQF_Thesis/9. APT Seminar/Returns_final.csv'
+data = datapreprocessing.Preprocessing(path)
+
+#%% Compute for each possible point in time within the data the n largest stocks based on market cap
+data.compute_nlargest(100)
+
+#%%
+lookback_window = 600
+rf              = 0.00
+date            = 20181231
+
+import optimizers as opt
+
+
+#%%
+permnos, market_weights, exp_returns, covars, returns_ahead = data.means_historical(date=date, lookback_window=lookback_window)
+    
+markowitz_pf      = opt.Markowitz(rf=rf, permnos=permnos, market_weights=market_weights, exp_returns=exp_returns, covars=covars)
+markowitz_weights = markowitz_pf.solve_weights()
+mc_weights        = markowitz_pf.solve_weights_mc()
+
+#%%
