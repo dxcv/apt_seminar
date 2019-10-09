09a45e2ad4b0965749e924601e14d0906d38930c
Silvano Marchesi <silvanom@slabhcib027.ethz.ch>
2019-10-09 17:32:04 +0200

    update

--- seminar.py
+++ seminar.py
@@ -2,41 +2,39 @@
 import numpy as np
 import pandas as pd
 import datetime
-
-
+import logging
 
 #%%
 df = pd.read_excel('Seminar_returns.xlsx')
 df.Datum = pd.to_datetime(df.Datum).dt.strftime('%Y%m%d')
 
-df.Datum
-
 
 #%%
-lookback_window     = 10
+
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+
+lookback_window     = 24
 rebal_period        = 3
 start_date          = '20190131'
 end_date            = '20190831'
 dates               = df.Datum.unique()
 start_position      = np.where(dates == start_date)[0].item(0)
 end_position        = np.where(dates == end_date)[0].item(0)
-tdates              = dates[start_position:end_position+1]
-odates              = dates[start_position-lookback_window+1:start_position+1]
-# tdates              = df.loc[(df.Datum >= start_date) & (df.Datum <= end_date),'Datum'].values
+obs_start           = dates[start_position-lookback_window+1]
+tdates              = df.loc[(df.Datum >= start_date) & (df.Datum <= end_date),'Datum'].values
+odates              = df.loc[(df.Datum >= obs_start) & (df.Datum <= end_date),'Datum'].values
 horizon             = len(tdates)
-max_rebal_period    = len(dates[start_position:end_position])
 
 
 if rebal_period == 1:
     mod = divmod(horizon, rebal_period)[0]
-elif rebal_period > max_rebal_period:
-    rebal_period = max_rebal_period
-    mod = divmod(horizon, rebal_period)[0]
+elif rebal_period > horizon:
+    rebal_period = 3
+    logging.warning('Rebalancing period exceeds horizon. Value set to 3 months')
+    mod = divmod(horizon, rebal_period)[0]+1
 else:
     mod = divmod(horizon, rebal_period)[0]+1
     
-
-print(tdates)
 #%%
 counter     = 0
 rebal_dates = []
@@ -47,12 +45,12 @@ for i in range(mod):
     counter += rebal_period
 print(rebal_dates)
 
-# #%%
-# for date in tdates:
-#     if date in rebal_dates:
-#         print(df.loc[(df.Datum <= date), 'Gold'].values)
-#     else:
-#         pass
+#%%
+for date in tdates:
+    if date in rebal_dates:
+        df.loc[(df.Datum == date), 'Gold'].values
+    else:
+        pass
 
 
 
