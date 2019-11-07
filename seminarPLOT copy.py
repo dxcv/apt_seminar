992f5943a6ba5666b2df08a554bde090187a3c3a
budfox3 <silvano.marchesi@uzh>
2019-11-07 23:19:14 +0100

    update

--- seminarPLOT copy.py
+++ seminarPLOT copy.py
@@ -1,4 +1,3 @@
-#%% Load packages
 import numpy as np
 import pandas as pd
 import datetime
@@ -8,12 +7,11 @@ import optimizers2
 import matplotlib.pyplot as plt
 import importlib
 
-#%% Load data
 df          = pd.read_excel('Seminar_returns.xlsx', sheet_name='time series')
 df_w        = pd.read_excel('Seminar_returns.xlsx', sheet_name='market caps')
 df.date     = pd.to_datetime(df.date).dt.strftime('%Y%m%d')
 
-#%%
+
 lookback_window     = 49
 rebal_period        = 3
 start_date          = '19780131'
@@ -45,8 +43,7 @@ for i in range(mod):
     rebal_dates.append(rebal_date)
     counter += rebal_period
 
-#%%
-# Initialize return arrays
+
 importlib.reload(optimizers2)
 m_return    = np.array([])
 ev_return   = np.array([])
@@ -56,15 +53,14 @@ rf_return   = np.array([])
 bl1_return  = np.array([])
 bl2_return  = np.array([])
 
-# Initialize weight differences
+
 W_bl1_W_saa = []
 W_bl2_W_saa = []
 
-# Define parameters for the BL models
 MeanModel1      = 'MLE'
 MeanMOdel2      = 'Holt'
 tau             = 0.02
-uncertainty     = 0.2
+uncertainty     = 0.8
 
 for date in tdates:
     # Create subset of data where there are at least as many values as required by the lookback window
@@ -109,7 +105,7 @@ for date in tdates:
         mybl2.get_model_return(tau=tau, P=P, O=O, q=q)
         _,_,W_bl2,_,_   = mybl2.efficient_frontier_bl()
 
-    # Update returns and portfolio weights 
+
     universe        = df.loc[(df.date == date)]
     returns_t       = np.squeeze(universe[universe.columns[universe.columns.isin(assets)]].values)
     
@@ -129,13 +125,7 @@ for date in tdates:
 
     rf_return       = np.append(rf_return, rf)
 
-    # # Collect weight differences from TAA to SAA
-    # if date in wdates:
-    #     W_bl1_W_saa.append(W_bl1-W_saa)
-    #     W_bl2_W_saa.append(W_bl2-W_saa)
 
-
-#%%
 m_value     = 1+m_return
 gmv_value   = 1+gmv_return
 ev_value    = 1+ev_return
@@ -150,9 +140,6 @@ saa_value   = np.cumprod(saa_value, axis=0)
 bl1_value   = np.cumprod(bl1_value, axis=0)
 bl2_value   = np.cumprod(bl2_value, axis=0)
 
-
-# labels = ['MV', '1/N', 'gMinVar', 'SAA', 'TAA(BL '+Model1+')', ]
-
 tdates2   = [datetime.datetime.strptime(str(x)[0:8], '%Y%m%d') for x in tdates]
 plt.plot(tdates2, m_value, label='MV', color='green')
 plt.plot(tdates2, ev_value, label='1/N', color='blue')
@@ -163,66 +150,4 @@ plt.plot(tdates2, bl2_value, label='TAA(BL '+MeanMOdel2+')', color='magenta')
 plt.title('Performance Chart with '+r'$\tau$='+str(tau).replace('\t', ' ')+' and $\omega$='+str(uncertainty))
 plt.legend()
 plt.yscale('log')
-
-
-#%%
-test1 = np.array(W_bl1_W_saa)
-
-#%%
-
-plt.plot(test1)
-
-#%%
-all_returns = [m_return, gmv_return, ev_return, saa_return, bl1_return, bl2_return]
-
-
-#%% Geometric Return
-all_values = [m_value, gmv_value, ev_value, saa_value, bl1_value, bl2_value]
-for i in all_values:
-    geometric_ret = (1+(i[-1]/i[0])**(1/len(i))-1)**12-1
-    print('gMean p.a.:'+str(geometric_ret))
- 
-#%% Standard Deviation
-for i in all_returns:
-    print('StDev p.a.: '+str(np.std(i)*np.sqrt(12)))
-
-#%% Value at Risk
-for i in all_returns:
-    print('1%-VaR: '+str(round(np.quantile(i, 0.01), ndigits=4)))
-
-# %% Expected Shortfall
-
-for i in all_returns:
-    vars = []
-    for alpha in np.linspace(0.00001, 0.01):
-        vars.append(round(np.quantile(i, alpha), ndigits=4))
-    es = np.mean(vars)
-    print('1%-'+'ES: '+str(np.round(es, 4)))
-
-# %% Sharpe Ratio
-def ann_sr(rets, rfrets):
-    return ((1+np.mean(rets-rfrets))**12-1)/(np.std(rets)*np.sqrt(12))
-
-for i in all_returns:
-    print('SR p.a.: '+str(ann_sr(i, rf_return)))
-
-#%% Kurtosis
-from scipy.stats import kurtosis, skew
-for i in all_returns:
-    print('Skewness: '+str(skew(i)))
-
-for i in all_returns:
-    print('Kurtosis: '+str(kurtosis(i)))
-
-#%%
-
-# %%
-height=W_bl2-W_saa
-bars = assets
-y_pos = np.arange(len(bars))
-plt.bar(y_pos, height)
-plt.xticks(y_pos, bars, rotation='vertical')
-
-
-
-# %%
+plt.show()
